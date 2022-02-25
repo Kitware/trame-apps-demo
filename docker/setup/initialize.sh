89c0b0e50e9e3b7b3f90cb9f5a305092480ded38
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2022-02-25 16:34:25 -0700

    wip

--- docker/setup/initialize.sh
+++ docker/setup/initialize.sh
@@ -1,3 +1,4 @@
 # Install app from local directory
 conda install -y "pytorch==1.9.1" "torchvision==0.10.1" -c pytorch
 conda install -y scipy "scikit-learn==0.24.2" "scikit-image==0.18.3" -c conda-forge
+pip install xaitk-saliency-demo trame-sandtank-xai trame-mnist pv-visualizer
