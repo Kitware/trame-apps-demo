56199956e6810394f65e9a647598a42e09c227ac
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2022-02-27 10:20:25 -0700

    fix typo

--- docker/setup/initialize.sh
+++ docker/setup/initialize.sh
@@ -1,4 +1,9 @@
 # Install app from local directory
 conda install -y "pytorch==1.9.1" "torchvision==0.10.1" -c pytorch
 conda install -y scipy "scikit-learn==0.24.2" "scikit-image==0.18.3" -c conda-forge
-pip install xaitk-saliency-demo trame-sandtank-xai trame-mnist pv-visualizer
+pip install    \
+    trame-mnist \
+    pv-visualizer \
+    trame-sandtank-ml \
+    trame-sandtank-xai \
+    xaitk-saliency-demo
